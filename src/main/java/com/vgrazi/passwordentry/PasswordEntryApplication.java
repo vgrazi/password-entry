873d650e40eb846be075506188405803aa73c6cd
vgrazi <vgrazi@gmail.com>
2024-11-22 10:42:36 -0500

    Initial commit

---
+++ src/main/java/com/vgrazi/passwordentry/PasswordEntryApplication.java
@@ -0,0 +1,13 @@
+package com.vgrazi.passwordentry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordEntryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordEntryApplication.class, args);
+	}
+
+}
